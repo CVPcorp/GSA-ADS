e913cab5c9973773a81f5c7c4cd11c804f54234e
chencvp <cchen@cvpcorp.com>
2015-06-22 15:48:21 -0400

    Jake's Changes

--- Includes/fdaApi.php
+++ Includes/fdaApi.php
@@ -1,7 +1,13 @@
 <?php
 function searchApi($term) {
-	$json = file_get_contents('https://api.fda.gov/drug/enforcement.json?search=' . $term . '&limit=4');
+    //print_r($term);
+    $queryString = array(
+        'search'=>$term[SearchTerm],
+        'search'=>$term[SearchTerm],
+        );
+	$json = file_get_contents('https://api.fda.gov/drug/enforcement.json?search=' . $term[SearchTerm] . '&limit=4');
 	$obj = json_decode($json);
-	return $obj->results;
+    return $obj->results;
 }
+
 ?>

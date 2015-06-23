fcfbf58160b567af8aa1e8a6de52d5aead616014
chencvp <cchen@cvpcorp.com>
2015-06-23 13:11:11 -0400

    Added headers and footers to create user

--- createUser.php
+++ createUser.php
@@ -1,30 +1,23 @@
 <?php include('Includes/userFunctions.php'); ?>
+<?php Include('Includes/header.php'); ?>
 <script type="text/javascript" src="Scripts/jquery-1.11.3.min.js"></script>
 <script type="text/javascript" src="Scripts/UserScripts.js"></script>
-<div id="results"></div>
-<table>
-<tr>
-	<td>User Name:</td>
-	<td>
-		<input type="text" id="UserName" />
-	</td>
-	</tr>
-	<tr>
-	<td>Password:</td>
-	<td>
-		<input type="Password" id="Password" />
-	</td></tr>
-	<tr>
-	<td>Email:</td>
-	<td>
-		<input type="text" id="Email" />
-	</td></tr>
-	<tr>
-		<td>
-			<input type="button" value="Submit" onclick="submitUser();" />
-		</td>
-		<td>
-		</td>
-	</tr>
-</tr>
-</table>
+    <div class="container">
+
+      <form class="form-signin">
+		<div id="results"></div>
+        <h2 class="form-signin-heading">Create an account</h2>
+        <label for="signUpEmail" class="sr-only">Email Address</label>
+        <input type="email" id="Email" class="form-control" placeholder="Email address" required>
+        <label for="inputUsername" class="sr-only">User Name</label>
+        <input type="text" id="UserName" class="form-control" placeholder="User Name" required autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="Password" class="form-control" placeholder="Password" required>
+        
+      
+        <button class="btn btn-lg btn-primary btn-block" type="submit" onclick="submitUser();">Sign Up</button>
+      </form>
+
+    </div> <!-- /container -->
+
+<?php Include('Includes/footer.php'); ?>
